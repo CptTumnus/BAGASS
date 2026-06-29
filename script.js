82c7396ebb55cceb6e5206c78ac7af398f3a551b
CptTumnus <mymembershipstw@gmail.com>
2026-06-29 21:47:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const btn=document.querySelector('.btn');
-const msgs=['ROLL THE DICE!','ONE MORE TURN!','LET THE GAMES BEGIN!'];
-let i=0;
-setInterval(()=>{i=(i+1)%msgs.length;btn.textContent=msgs[i]},2500);
