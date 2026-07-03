fa7949cd3af33011342b16ca86aabeb2558ae978
CptTumnus <mymembershipstw@gmail.com>
2026-07-03 14:39:46 +0100

    Update paul-exe.js

--- paul-exe.js
+++ paul-exe.js
@@ -1,1 +1,23 @@
+let percent = 0;
 
+const bar = document.getElementById("fill");
+
+const text = document.getElementById("percent");
+
+const timer = setInterval(() => {
+
+    percent++;
+
+    bar.style.width = percent + "%";
+
+    text.innerHTML = percent + "%";
+
+    if(percent >= 100){
+
+        clearInterval(timer);
+
+        text.innerHTML = "Champion Loaded";
+
+    }
+
+},40);
